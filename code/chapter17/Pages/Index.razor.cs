7c1f691f869b2c1e549bde6de13b9e8b67e0909b
Ricardo Peres <rjperes@hotmail.com>
2020-06-05 17:00:40 +0100

    Contents

---
+++ code/chapter17/Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿using chapter17.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chapter17.Pages
+{
+    //[Route("/")]
+    [Layout(typeof(MainLayout))]
+    public partial class Index : ComponentBase
+    {
+    }
+}
---
+++ code/chapter17/Pages/SimpleComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chapter17.Pages
+{
+    public partial class SimpleComponent : ComponentBase
+    {
+    }
+}
